842aa75d7f6a2416d3b45907b25ffa7768129074
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-15 15:02:59 +0300

    Set up Alert Controller with handler

--- LogInDemo/LogInViewController.swift
+++ LogInDemo/LogInViewController.swift
@@ -12,12 +12,40 @@ class LogInViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    private let userName = "User"
+    private let password = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func forgotButtonPressed(_ sender: UIButton) {
+        var message = "Your name is \(userName)"
+        
+        if sender.tag == 1 {
+            message = "Your password is \(password)"
+        }
+        
+        showAlert(title: "Ooops!",
+                  message: message) { _ in
+            self.dismiss(animated: true)
+        }
+    }
+    
+    private func showAlert(title: String, message: String, buttonHandler: ((UIAlertAction) -> Void)?) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "OK",
+                                        style: .default,
+                                        handler: buttonHandler ?? nil)
+        
+        alert.addAction(alertAction)
+        
+        present(alert, animated: true)
+    }
 }
-
 /*
  Palette:
  #1: D9F8C4
